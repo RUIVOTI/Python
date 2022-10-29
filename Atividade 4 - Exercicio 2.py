7372fd18ed0e896d937777dd73bd924746de74d5
RUIVOTI <lucashortamoreira6@gmail.com>
2022-10-28 21:28:51 -0300

    Atividade 4-Exercicio 1

---
+++ Atividade 4 - Exercicio 2.py
@@ -0,0 +1,20 @@
+# Notas Do Ensino Medio
+
+print("Notas")
+PROV1 = float(input("Digete a nota da primeiro da prova: "))
+PROV2 = float(input("Digete a nota da segunda prova: "))
+PROV3 = float(input("Digete a nota da terceira prova: "))
+PROV4 = float(input("Digete a nota da quarta prova: "))
+
+TRA1 = float(input("Digite a nota do primeiro trabalho: "))
+TRA2 = float(input("Digete a nota do segundo trabalho: "))
+TRA3 = float(input("Digete a nota do terceiro trabalho: "))
+TRA4 = float(input("Digete a nota do quarto trabalho: "))
+
+valor = PROV1 + TRA1
+valor = PROV2 + TRA2
+valor = PROV3 + TRA3
+valor = PROV4 + TRA4
+valortotal = PROV1 + TRA1 + PROV2 + TRA2 + PROV3 + TRA3 + PROV4 + TRA4
+
+if =
