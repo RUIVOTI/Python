04c8c7a47eb5e3106e9f1aa487646df2f137e630
RUIVOTI <lucashortamoreira6@gmail.com>
2022-10-31 19:54:45 -0300

    Atividade4-Exercicio3

--- Atividade 4 - Exercicio 1.py
+++ Atividade 4 - Exercicio 1.py
@@ -1,15 +1,18 @@
 # Notas Do Ensino Medio
 
 print("Notas")
-B1 = float(input("Digite a nota do primeiro bimestre: "))
-B2 = float(input("Digite a nota do segundo bimestre: "))
-B3 = float(input("Digite a nota do terceiro bimestre: "))
-B4 = float(input("Digite a nota do quarto bimestre: "))
+B1 = float(input("Digite a nota do primeiro bimestre (Valor máximo 25): "))
+B2 = float(input("Digite a nota do segundo bimestre (Valor máximo 25): "))
+B3 = float(input("Digite a nota do terceiro bimestre (Valor máximo 25): "))
+B4 = float(input("Digite a nota do quarto bimestre (Valor máximo 25): "))
 
 total = float(B1 + B2 + B3 + B4)
+
 if(total >=60 and total <=100):
     print("Você foi aprovado")
 elif(total >=40 and total <60):
     print("Você esta de recuperação")
 elif(total >=0 and total <40):
     print("Você foi reprovado")
+else:
+    print("Confira o valor digitado, valor invalido!")
--- Atividade 4 - Exercicio 3.py
+++ Atividade 4 - Exercicio 3.py
@@ -1,3 +1,20 @@
 # Matriz de 5 linhas e 5 colunas.
 
-matriz = [[1,2,3,4], [1,2,3,4], [1,2,3,4], [1,2,3,4], [1,2,3,4]]
+lista = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0,], [0,0,0,0,0], [0,0,0,0,0]]
+
+for i in range(5):
+    for j in range(5):
+        lista[i][j]=int(input("Digete um numero inteiro: "))
+
+n1 = int(input("Digite um numero para localizar a matriz"))
+encontrado = False
+for i in range(5):
+    for j in range(5):
+        if(lista[i][j] == n1):
+            print("nuemro encontrado", n1, "na posição", lista,i, j)
+            encontrado = True
+if(encontrado is False):
+    print("Nao foi encontrado na matriz seu numero")
+
+
+
