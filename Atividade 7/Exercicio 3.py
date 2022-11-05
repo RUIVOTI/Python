0f57c1ad603db9ec2f5bdf8f427f09d734c7f9ff
RUIVOTI <lucashortamoreira6@gmail.com>
2022-11-04 21:36:35 -0300

    Atividade 7

--- Atividade 7/Exercicio 3.py
+++ Atividade 7/Exercicio 3.py
@@ -4,8 +4,9 @@ def fatorial(n: int) -> int:
     if n == 1 or n == 2:
         return 1
     return n * fatorial(n - 1)
+n = int(input("Digete um numero: "))
 
-
-if __name__ == "__main__":
-    fat5 = fatorial(5)
-    print(fat5)
+print("Fatorial de",n)
+for i in range(n):
+    n = fatorial(i+1)
+    print(i+1,"-",n)
